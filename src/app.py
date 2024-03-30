4f044ce31b4a1388c905774528dbf0a396e6beff
pillow-tech <tj.contreras24@gmail.com>
2024-03-30 19:01:07 +0800

    New endpoint for the simple-flask webserver

--- src/app.py
+++ src/app.py
@@ -5,3 +5,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/up_check")
+def up_check():
+    return "OK"
+
+@app.route("/test_endpoint")
+def test_endpoint():
+    msg = ""
+    msg = msg + "<p>Hello, World!</p>"
+    msg = msg + "<p>This is a new endpoint for simple flask webserver!</p>"
+    return msg
